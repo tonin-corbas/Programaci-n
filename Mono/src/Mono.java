1202108dc36c5fe6f2ea70824ce3f551c28fbc5b
Toñin <j.antoniokumar@gmail.com>
2023-11-14 16:35:35 +0100

    Parche

---
+++ Mono/src/Mono.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int valorDado = r.nextInt(10);
+        System.out.println(valorDado);
+
+        if (valorDado % 2){
+
+        }
+    }
+}
