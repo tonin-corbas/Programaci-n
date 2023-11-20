16e372d06155e52aa33828b218a3b1956d50aeda
Toñin <j.antoniokumar@gmail.com>
2023-11-20 19:05:01 +0100

    Parche

--- EJ2/src/EleccionAleatoria.java
+++ EJ2/src/EleccionAleatoria.java
@@ -8,17 +8,26 @@ public class EleccionAleatoria {
         int num = r.nextInt(0,50);
         int cont = 0;
         Scanner scan = new Scanner(System.in);
-        System.out.println("Elige un número del 1 al 50.");
+        System.out.println("Elige un número del 0 al 50.");
         int elec = scan.nextInt();
-        while (cont < 4){
-            if (elec == num){
+
+        boolean fin = false;
+        while (cont < 4 && !fin){
+            if (elec == num) {
                 System.out.println("Has acertado");
-            }else {
+                fin = true;
+            } else if (elec > num) {
                 System.out.println("has fallado");
-                System.out.println("Vuelve a elegir otro número");
+                System.out.println("Vuelve a elegir otro número menor a " + elec);
                 elec = scan.nextInt();
-            }
-            cont++;
-         }
+            } else if (elec < num) {
+                System.out.println("has fallado");
+                System.out.println("Vuelve a elegir otro número mayor a " + elec);
+                elec = scan.nextInt();
+            }cont++;
+        }
+        if (fin == false){
+            System.out.println("El número elegido era " + num);
+        }
     }
 }
