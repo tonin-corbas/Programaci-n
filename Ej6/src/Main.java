c5aa643c01ff58f5166ab38dd7f41a328878fce7
Toñin <j.antoniokumar@gmail.com>
2023-12-14 19:02:43 +0100

    Parche

--- Ej6/src/Main.java
+++ Ej6/src/Main.java
@@ -2,19 +2,19 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> numeros = new ArrayList<Integer>();
-        numeros.add(30);
-        numeros.add(13);
-        numeros.add(69);
-        numeros.add(67);
-        numeros.add(14);
-        numeros.add(26);
-        numeros.add(43);
-        numeros.add(54);
-        numeros.add(32);
+        ArrayList<String> listaDias = new ArrayList<String>();
+        listaDias.add("Lunes");
+        listaDias.add("Martes");
+        listaDias.add("Miercoles");
+        listaDias.add("Jueves");
+        listaDias.add("Viernes");
+        listaDias.add("Sábado");
+        listaDias.add("Domíngo");
+        listaDias.set(4, "Juernes");
+        ArrayList<String> listaDos = new ArrayList<String>(listaDias);
+        System.out.println(listaDias.get(3) + ", " + listaDias.get(4));
+        String primer = listaDias.get(0);
+        String ultimo = listaDias.get(listaDias.size()-1);
 
-        for (int i : numeros) {
-            System.out.println(i);
-        }
     }
 }
